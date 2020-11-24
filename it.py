999ff222d1c12769cb9b4c127a2ec659cae356cc
Macuway <71066639+macuway@users.noreply.github.com>
2020-11-24 11:49:00 +0200

    Ma näen sind läbi sinu veebikaamera.
    M a  n ä e n  k õ i k e

---
+++ it.py
@@ -0,0 +1,8 @@
+tmp = int(input("Temp: "))
+
+if tmp < 0:
+    print("Kas sind küüditati Siberisse?")
+if tmp > 30:
+    print("Stalin arvab, et sa läksid praeahju...")
+else:
+    print("Stalin kiidab temperatuuri heaks!")
---
+++ owo.py
@@ -0,0 +1,19 @@
+print("Hello Africa!")
+
+print(5 + 3)
+print("Hello, " + "World!")
+
+user_name = input("Mis on sinu nimi:").capitalize()
+user_friend_name = "bob"
+
+print("Tere, " + user_name + "!")
+
+print(user_friend_name.endswith('o'))
+
+hotel = "Trivago"
+
+hotel = input("Hotel? ").capitalize()
+
+print(hotel.endswith('o'))
+
+print("Hotel on alati " + bruh + "!")
